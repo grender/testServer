6a161d466e0652cee7b84ef968a49bb59ba42ebf
Alexander Kopytov <grender.no8@gmail.com>
2014-09-18 15:01:43 +0400

    request refactor

--- server.js
+++ server.js
@@ -1,73 +1,94 @@
 var https = require('https');
 var connect = require('connect');
-var util=require('util');
+var util = require('util');
+var connectRoute = require("connect-route");
 
-var ListenPort=10572;
-var SendingRequestPort=12560;
+var ListenPort = 10572;
+var SendingRequestPort = 443;
 
-function routes(app){
-    app.get('*', saveToFile);
-    app.post('*', saveToFile);
+function routes(app) {
+	app.get('*/*', saveToFile);
+	app.post('*/*', saveToFile);
 }
 
-var server = connect.createServer()
-//							.use(connect.logger())
-							.use(connect.router(routes));
-    server.listen(ListenPort);
+var server = connect()
 
-var respNo=0;
+//server.use(connect.logger())
+// server.use(connectRoute(routes));
+server.use(saveToFile);
+server.listen(ListenPort);
 
-function print(respNo,str)
-{
+var respNo = 0;
+
+function print(respNo, str) {
 	str.split("\n").forEach(function(s) {
-	    console.log("Response NO:"+respNo,s);
+		console.log("Response NO:" + respNo, s);
 	});
-	
-}
-							
-function saveToFile(req,res,next)
-{
-	respNo=respNo+1;
-	print(respNo,util.format('%s %s %j',req.method,req.url,req.headers));
 
-	var requestToNginx=https.request(
-	{
-		    hostname: '127.0.0.1',
-		    port: SendingRequestPort,
-		    path: req.url,
-		    method: req.method
-		},function(nginxRes) {
-			print(respNo,util.format('nginx headers:%j',nginxRes.headers));
-			res.writeHead(nginxRes.statusCode,nginxRes.headers);
-			var data="";
-		nginxRes.on('data', function (chunk) {
-			data=data+chunk;
+}
+
+function saveToFile(req, res, next) {
+	respNo = respNo + 1;
+	
+	console.log("VVVVVV"+ respNo + "VVVVVV")
+	// print(respNo, util.format('%s %s %j', req.method, req.url, req.headers));
+
+
+	req.headers.host='t.srv4pos.com'
+	
+	//if(!req.headers.accept)
+	//	req.headers.accept="image/png,image/*;q=0.8,*/*;q=0.5"
+		
+	console.log("---request Header")
+	for(h in req.headers) {
+		console.log(h+":"+req.headers[h])
+	}
+	var requestToNginx = https.request({
+		hostname : 't.srv4pos.com',
+		port : SendingRequestPort,
+		path : req.url,
+		method : req.method,
+		headers: req.headers
+	}, function(nginxRes) {
+		// print(respNo, util.format('nginx headers:%j', nginxRes.headers));
+		console.log("---response Header")
+		for(h in nginxRes.headers) {
+			console.log(h+":"+nginxRes.headers[h])
+		}
+		res.writeHead(nginxRes.statusCode, nginxRes.headers);
+		var data = "";
+		nginxRes.on('data', function(chunk) {
+			data = data + chunk;
 			res.write(chunk);
-			});
-		nginxRes.on('end', function () {
-			    print(respNo,util.format('Result code:%d', nginxRes.statusCode));
-			    print(respNo,util.format('%s', data));
-				res.end();
-			});
-			nginxRes.on('error',function(e) {
-				print(respNo,util.format('Nginx response error:%j', e));
-			});
-	    });
-	requestToNginx.on('error',function(e) {
-	    print(respNo,util.format('Nginx request error:%j', e));
+		});
+		nginxRes.on('end', function() {
+			// print(respNo, util.format('Result code:%d', nginxRes.statusCode));
+			// print(respNo, util.format('%s', data));
+			res.end();
+			console.log("^^^^^^"+ respNo + "^^^^^^")
+		});
+		nginxRes.on('error', function(e) {
+			// print(respNo, util.format('Nginx response error:%j', e));
+			console.log("^^^^^^"+ respNo + "^^^^^^")
+		});
 	});
-	if(req.method=="POST") {
-		var data="";
-		req.on('data', function (chunk) {
-			data=data+chunk;
+	requestToNginx.on('error', function(e) {
+		// print(respNo, util.format('Nginx request error:%j', e));
+		console.log("^^^^^^"+ respNo + "^^^^^^")
+	});
+	if (req.method == "POST") {
+		var data = "";
+		req.on('data', function(chunk) {
+			data = data + chunk;
 			requestToNginx.write(chunk);
 		});
-		req.on('end', function () {
-		    requestToNginx.end();
-		    print(respNo,util.format('POST DATA:%s', data));
+		req.on('end', function() {
+			requestToNginx.end();
+			// print(respNo, util.format('POST DATA:%s', data));
 		});
-	}else
+	} else {
 		requestToNginx.end();
+	}
 	//next();
-	
+
 }
